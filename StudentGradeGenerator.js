14456c3ecc4e618bd999b8f695f9ffaa6af099d3
farhiya21 <farhiya.bashir@student.moringaschool.com>
2023-06-18 22:31:55 +0300

    commit message

--- StudentGradeGenerator.js
+++ StudentGradeGenerator.js
@@ -1,1 +1,13 @@
-#Code-challenges
+const gradeGenerator = function (num) {
+    if (num > 79) {
+      return `You have an A`;
+    } else if (num >= 60 && num <= 79) {
+      return `You have a B`;
+    } else if (num >= 49 && num <= 59) {
+      return `You have a C-`;
+    } else if (num >= 40 && num < 49) {
+      return `You have a D-`;
+    } else if (num < 40) {
+      return `You have an E`;
+    }
+  };
